a7107c90ef4adfae92842cdf3fc619763540ba56
ItzAnsh <anshbhasin3723@gmail.com>
2022-10-25 23:51:47 +0530

    lil changes

--- index.js
+++ index.js
@@ -8,78 +8,72 @@ app.use(cors());
 app.set("view engine", "ejs");
 
 app.get("/", async (req, res) => {
-  return res.render("index");
+	return res.render("index");
 });
 //let sheetId = "1hS64YBsANmCnCS0ebhUPuhwmUap755dZXLs8orcaVho";
 let sheetId = "1teS758X1OgRCoHEHNWcZE_77XU5flHoJyCT5ikyeQ80";
 
 app.get("/get-data", async (req, res) => {
-  try {
-    console.log("getting data");
+	try {
+		console.log("getting data");
 
-    let responseData = await axios.get(
-      `https://holy-sheet-api.herokuapp.com/A2/B120000000/${sheetId}`
-    );
+		let responseData = await axios.get(
+			`https://holy-sheet-api.herokuapp.com/A2/B120000000/${sheetId}`
+		);
 
-    let entries = responseData.data.values;
+		let entries = responseData.data.values;
 
-    let backgroundColorData = [
-      "rgb(30,144,255)",
-      
-    ];
+		let backgroundColorData = ["rgb(30,144,255)"];
 
-    let borderColorData = [
-      "rgb(30,144,255)",
-     
-    ];
+		let borderColorData = ["rgb(30,144,255)"];
 
-    let labels = [];
-    let values = [];
+		let labels = [];
+		let values = [];
 
-    entries.map((entry) => {
-      labels.push(entry[0]);
-      values.push(entry[1]);
-    });
+		entries.map((entry) => {
+			labels.push(entry[0]);
+			values.push(entry[1]);
+		});
 
-    let backgroundColor = [];
-    let borderColor = [];
-    let counter = 0;
-    for (let i = 0; i < labels.length; i++) {
-      backgroundColor.push(backgroundColorData[counter]);
-      borderColor.push(borderColorData[counter]);
-      counter++;
-      if (counter === 6) {
-        counter = 0;
-      }
-    }
+		let backgroundColor = [];
+		let borderColor = [];
+		let counter = 0;
+		for (let i = 0; i < labels.length; i++) {
+			backgroundColor.push(backgroundColorData[counter]);
+			borderColor.push(borderColorData[counter]);
+			counter++;
+			if (counter === 6) {
+				counter = 0;
+			}
+		}
 
-    console.log("labels are ", labels, " values are ", values);
+		console.log("labels are ", labels, " values are ", values);
 
-    let finObj = {};
+		let finObj = {};
 
-    let dataSet = [
-      {
-        label: "Time X Signal strength Graph",
-        data: values,
-        backgroundColor: backgroundColor,
-        borderColor: borderColor,
-        borderWidth: 1,
-      },
-    ];
+		let dataSet = [
+			{
+				label: "Time X Signal strength Graph",
+				data: values,
+				backgroundColor: backgroundColor,
+				borderColor: borderColor,
+				borderWidth: 1,
+			},
+		];
 
-    finObj.labels = labels;
-    finObj.dataSet = dataSet;
+		finObj.labels = labels;
+		finObj.dataSet = dataSet;
 
-    // return res.send(responseData.data);
-    return res.send(finObj);
-  } catch (e) {
-    console.log("error", e);
-    return res.send({});
-  }
+		// return res.send(responseData.data);
+		return res.send(finObj);
+	} catch (e) {
+		console.log("error", e);
+		return res.send({});
+	}
 });
 
 // app.listen(8000, () => {
 //   console.log("server running on port 8000");
 // });
 
-app.listen(process.env.PORT || 8000)
+app.listen(process.env.PORT || 8000);
